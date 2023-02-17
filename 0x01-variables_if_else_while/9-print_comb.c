6c507111122af6719256033224a64a9ac67ac3da
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-02-17 11:08:04 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,15 +10,17 @@ int main(void)
 {
 	int n;
 
-	for (n = 0 ; n < 10 ; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n = '0');
-		if (n < 9)
-		{
-			putchar(',');
-			putchar(32);
-		}
+		putchar((n % 10) + '0');
+		if (n == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
