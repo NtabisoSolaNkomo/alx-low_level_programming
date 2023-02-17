3e4f9dacbfc4399ffb1db866aba4325ff3b99329
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-02-17 09:50:42 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, expect for q and e
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		if ((c == 'q' || c == 'e') != 1)
+		{
+			put char(c);
+		}
+
+	}
+	putchar('\n');
+	return (0);
+}
