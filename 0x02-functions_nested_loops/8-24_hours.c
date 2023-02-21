1c42ca6d2f713a1233a1c997f60b9a4d12376b67
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-02-21 09:19:18 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,10 +12,10 @@ void jack_bauer(void)
 
 	i = 0;
 	
-	while (i < 24)
+	for (i = 0; i < 24; i++)
 	{ 
 		j = 0;
-		while (j < 60)
+		for (j = 0; j < 60; j++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
@@ -23,8 +23,6 @@ void jack_bauer(void)
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 			_putchar('\n');
-			j++;
 		}
-		i++;
 	}
 }
