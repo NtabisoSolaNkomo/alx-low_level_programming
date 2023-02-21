6954eb1b5ddf296df550b9ef822deaedd93535c1
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-02-21 10:38:10 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@
  */
 
 void print_to_98(int n)
-{
+	{
 	if (n < 98)
 	{
 		while (n <= 98)
@@ -16,19 +16,19 @@ void print_to_98(int n)
 			printf("%d", n);
 			if (n != 98)
 			{
-				printf(",");
+				printf(", ");
 			}
 			n++;
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		while (n >= 98)
 		{
 			printf("%d", n);
 			if (n != 98)
 			{
-				printf(",");
+				printf(", ");
 			}
 			n--;
 		}
