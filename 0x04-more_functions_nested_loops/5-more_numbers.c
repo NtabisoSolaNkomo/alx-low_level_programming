ce7f2ca7ce771a5be0e9601bd2b045ae9883d4c3
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-02-23 07:06:22 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,15 @@
 
 void more_numbers(void)
 {
-	int i, ro;
+	int i, j;
 
-	for (ro = 0; ro < 10; ro++)
+	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i<= 14; i++)
 		{
 			if (i >= 10)
-			{
 				_putchar((i / 10) + '0');
-			}
-			_putchar((i % 10) + '0');
+				_putchar((i % 10) + '0');
 		}
 		_putchar('\n')
 	}
