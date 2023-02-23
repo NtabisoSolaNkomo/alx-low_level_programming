8803b0ea7d7bf90f56206f5da49896cfc76cfe07
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-02-23 00:31:26 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - function that prints a square
+ *
+ * Return:
+ */
+
+void print_square(int size)
+{
+	int co, ro;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (co = 1; co <= size; co++)
+		{
+			_putchar('#');
+			for (ro = 2; ro <= size; ro++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
