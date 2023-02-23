5e4c7d6bd26f9465b236d2c91a69fb706ef7656f
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-02-23 01:58:37 -0800

    1-main

---
+++ 1-main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/** 
+ * main - infinite loop
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinitee loop coming :(\n");
+
+	i = 0;
+
+	printf("Infinite loop avoid! \\o/\n");
+	
+	return (0);
+}
