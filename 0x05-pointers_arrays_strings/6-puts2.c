b62fbf0b501268b6cc18267970477547458d72c9
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-02-27 06:16:39 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/*
+ * main - prints every other character of a string
+ *
+ * Return:
+ */
+void puts2(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[0]);
+	}
+	}
+	_putchar('\n');
+}
