9360ae928cdb7b84fb476b2352c1103574fb9630
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-03-06 09:55:24 -0800

    4-strpbrh

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * *_strpbrk - searches a string for any of a set of bytes
  * @s: string to search
@@ -24,5 +25,5 @@ char *_strpbrk(char *s, char *accept)
 		s++;
 	}
 
-	return (NULL)l
+	return (NULL);
 }
