10237df00d24d1cd27e0745a6fd0415c35198574
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-03-16 05:32:05 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _calloc
+ * @nmemb
+ * @size
+ *
+ * Return:
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (size >= UINT_MAX / nmemb || nmemb >= UINT_MAX / size)
+		return (NULL);
+	p = malloc(size * nmemb);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		p[i] = 0;
+	return ((void *)p);
+}
