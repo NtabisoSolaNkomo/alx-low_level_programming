13ac329bd0efdb0099b8d4a9cfe4732e24a54647
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-03-23 11:05:49 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,7 +7,7 @@
  *
  * Return: void
  */
-void print_number(const char *separator, const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
