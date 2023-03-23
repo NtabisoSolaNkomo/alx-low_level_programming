f0dc632e5054d156f44017b3e8725d9759927ffe
NtabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-03-23 11:17:48 -0700

    {

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(valist, char *);
-		if (str == NILL)
+		if (str == NULL)
 		{
 			str = "(nil)";
 		}
