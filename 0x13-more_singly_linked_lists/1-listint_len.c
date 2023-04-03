aba3b03b3ab7c045c4070130db8dbd051e81807f
ntabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-04-03 09:54:08 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listin_len
+ *
+ * Return:
+ */
+size_t listin_len(const listint_t *h)
+{
+	int len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
