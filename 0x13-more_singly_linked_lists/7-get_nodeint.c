c917eed221c379815bac243ffc1bbdba6e494d25
ntabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-04-05 07:27:15 -0700

    7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index
+ * @head
+ * @index
+ *
+ * Return: index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+		return (0);
+
+	while (head)
+	{
+		if (index == i)
+			return (head);
+		i++;
+		head = head->next;
+	}
+	return (0);
+}
