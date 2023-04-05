ea4525b132b03e9230e063ef677eae184c790b45
ntabisoSolaNkomo <ntabisonkomo111@gmail.com>
2023-04-05 07:29:25 -0700

    sum_listint

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint
+ * @head
+ *
+ * Return: sum
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (sum);
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
